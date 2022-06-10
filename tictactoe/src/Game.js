1d0c76709bfd24eede9d9331800c4403ed1c44c6
Ismael de Laet Abashi <ismaelabashi@hotmail.com>
2022-06-10 17:03:27 -0300

    Feat(Game): Add function calculateWinner

--- tictactoe/src/Game.js
+++ tictactoe/src/Game.js
@@ -13,4 +13,33 @@ export default class Game extends React.Component {
 			</div>
 		);
 	}
+
+	calculateWinner(squares) {
+		const linesToWin = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+
+		const isWinner = () => {
+			for (let i = 0; i < linesToWin.length; i++) {
+				const [a, b, c] = linesToWin[i];
+				const symbolsMatch =
+					squares[a] &&
+					squares[a] === squares[b] &&
+					squares[a] === squares[c];
+
+				if (symbolsMatch) {
+					return squares[a];
+				}
+			}
+
+			return null;
+		};
+	}
 }
